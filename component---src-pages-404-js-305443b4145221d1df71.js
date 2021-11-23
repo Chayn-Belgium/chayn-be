b73f287764f347769d011c5a3759ec9d94fb5141
KevinTss <kevin.tassi@gmail.com>
2021-11-23 22:44:09 +0000

    Deploying to gh-pages from  @ 6a29e0b085f5298874bf61f98f53a4f01e81b9c7 🚀

---
+++ component---src-pages-404-js-305443b4145221d1df71.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),w=t.n(u);e.default=function(){return w.a.createElement("div",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-305443b4145221d1df71.js.map
