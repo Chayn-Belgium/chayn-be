5f5ba181d8567703b64fab9dcb2ff4df3360bc04
KevinTss <kevin.tassi@gmail.com>
2021-12-28 16:26:08 +0100

    Add guide chapter pages

---
+++ src/pages/fr/guides/les-premiers-gestes-pour-affronter-une-situation-de-cyberharcelement/ressources/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import GuidePage from "../../../../../components/guide/guide-page"
+import { GUIDE } from "../../../../../../site-data/constants"
+
+const Page = () => (
+  <GuidePage
+    lang="fr"
+    name={GUIDE.THE_FIRST_STEPS_TO_FACE_A_CYBERBULLYING_SITUATION}
+    chapterId="ressources"
+  />
+)
+
+export default Page
